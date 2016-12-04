9b5b33122b2eb615f90e5d01d05151075cf2f695
Matthew Ramadan <matthewramadan@Matthews-MacBook-Pro.local>
2016-12-04 14:35:06 -0800

    Refactored using include instead of one large if statement

--- count_vowels/count_vowels.rb
+++ count_vowels/count_vowels.rb
@@ -1,13 +1,20 @@
 # Count vowels
 # Matthew Ramadan 2016
 
+# Get string from user that we're going to count the numbers of vowels in
 print "Enter a string and I'll count the amount of vowels in it: "
 user_string = gets.chomp
 
+# Scan each letter and add to array
 user_string_scan = user_string.scan /\w/
+
+# Used to count the number of vowels in string
 vowel_counter = 0
+
+# Iterate through each letter and check whether it's a vowel
 user_string_scan.each do | x |
-  vowel_counter += 1 if x == "a" || x == "e" || x == "i" || x == "o" || x == "u"
+  vowel_counter += 1 if ["a","e", "i", "o", "u"].include?(x)
 end
 
+# Display number of vowels to user
 puts "There are #{vowel_counter} vowels in \"#{user_string}\""
